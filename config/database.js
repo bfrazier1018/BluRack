1fd5e304db5f4e88e318b68f51446516815b8ff1
bfrazier1018 <bfrazier1018@gmail.com>
2018-08-02 19:19:45 -0400

    Add mongodb production url

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: 'mongodb://localhost:27017/BluRack'
+	url: 'mongodb://localhost:27017/BluRack' || 'mongodb://heroku_8tgfqwv5:8pb05ngftqj9vo5lfgqm6cf1jq@ds111492.mlab.com:11492/heroku_8tgfqwv5'
 };
