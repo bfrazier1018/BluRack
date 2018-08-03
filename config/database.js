f967609de859639089d2c5b7940287dc9ee1dbc4
bfrazier1018 <bfrazier1018@gmail.com>
2018-08-03 12:23:32 -0400

    Lighten background images

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	// url: 'mongodb://localhost:27017/BluRack'
-	url: 'mongodb://heroku_8tgfqwv5:8pb05ngftqj9vo5lfgqm6cf1jq@ds111492.mlab.com:11492/heroku_8tgfqwv5'
+	url: 'mongodb://localhost:27017/BluRack'
+	// url: 'mongodb://heroku_8tgfqwv5:8pb05ngftqj9vo5lfgqm6cf1jq@ds111492.mlab.com:11492/heroku_8tgfqwv5'
 };
